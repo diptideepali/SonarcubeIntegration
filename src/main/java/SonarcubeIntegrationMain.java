5a6aa4e905662985214d1e47e0bbf784224eaa9e
diptideepali <dipti_tiwari@naxgrp.com>
2023-08-27 19:13:17 +0530

    changes done for sonarcube

--- src/main/java/SonarcubeIntegrationMain.java
+++ src/main/java/SonarcubeIntegrationMain.java
@@ -1,13 +1,11 @@
-package sonarcubeIntegration;
+import sonarcubeIntegration.Calculator;
 
 public class SonarcubeIntegrationMain {
 
     public static void main(String[] args) {
-        Calculator calc = new calculator();
+        Calculator calc = new Calculator();
         System.out.println("hello world..........!");
         System.out.println(calc.add(2,4));
         System.out.println(calc.multi(2,4));
-        System.out.println(calc.square(3));
-        System.out.println(calc.cube(3));
     }
 }
